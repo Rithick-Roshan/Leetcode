02dedf8a58d4f83f7ece6f9292de3094aaedd845
RITHICK ROSHAN S <rithick1887@gmail.com>
2024-09-25 22:20:28 +0530

    up

---
+++ 389. Find the Difference/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+    int res =0;
+    for(int i=0;s[i]!='\0';i++){
+        res^=s[i];
+    }
+    for(int i=0;t[i]!='\0';i++){
+        res^=t[i];
+    }
+    char ch=res;
+    return ch;
+    }
+};
